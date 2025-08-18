60559ab2859e23798985f831ea4a7276ac29bfa8
Erin Sarlak <erinsarlak003@gmail.com>
2025-08-18 21:19:38 +0300

    added   db.create_all()

--- defect-tracker-backend/app.py
+++ defect-tracker-backend/app.py
@@ -119,6 +119,9 @@ def create_app():
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), debug=debug_mode)
