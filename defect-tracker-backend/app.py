a596ab76158e0b0263c9e172eb1aea4dc1cdb910
Erin Sarlak <erinsarlak003@gmail.com>
2025-08-18 19:33:56 +0300

    Procfile update

--- defect-tracker-backend/app.py
+++ defect-tracker-backend/app.py
@@ -113,8 +113,9 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), debug=debug_mode)
 
